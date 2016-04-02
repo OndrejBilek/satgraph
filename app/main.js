135f2faad951919d5f998063b40c8d14eda002a9
Ondřej Bílek <Bilcuus@gmail.com>
2016-04-02 11:32:19 +0200

    New icons, handle squirell events

--- app/main.js
+++ app/main.js
@@ -1,14 +1,39 @@
 'use strict';
 
 const app = require('app');
+const dialog = require('electron').dialog;
 const BrowserWindow = require('browser-window');
 
 let mainWindow;
 
 app.on('ready', function() {
+  handleStartupEvent();
   createWindow();
   addListeners();
 });
+
+function handleStartupEvent(){
+  if (process.platform !== 'win32') {
+    return;
+  }
+
+  var squirrelCommand = process.argv[1];
+  switch (squirrelCommand) {
+    case '--squirrel-install':
+      dialog.showErrorBox("Install", "Install");
+      app.quit();
+      return;
+    case '--squirrel-updated':
+      app.quit();
+      return;
+    case '--squirrel-uninstall':
+      app.quit();
+      return
+    case '--squirrel-obsolete':
+      app.quit();
+      return;
+  }
+};
 
 function createWindow() {
   mainWindow = new BrowserWindow();
